580b90dd6df8e3bf71dda77b5a111fe57b8a23b7
bowlsorice <69162598+bowlsorice@users.noreply.github.com>
2020-09-25 14:33:46 -0500

    Gave up on art for now- made prototype slingshot

--- anger_birbs.py
+++ anger_birbs.py
@@ -9,11 +9,6 @@ from Box2D.b2 import (world, polygonShape, circleShape,
 #DEFINE CLASSES, INIT AND DRAW METHODS FIRST
 #CLASSES: BIRD, ENEMY(PIG), SCENERY, LEVEL - WHAT SHOULD SLINGSHOT BE?
 
-#what I learned 9/22:
-#rotation is destructive, each rotation should reference the og image
-# bird designs must be more circular!!
-
-#bird radius is .2 m standard - ??
 
 from anger_sprites import *
 
@@ -26,7 +21,7 @@ BLACK = (0,0,0)
 
 game = True
 running = True
-art = True
+art = False
 
 rotate = 0
 
@@ -36,6 +31,7 @@ screen = pygame.display.set_mode((VIEW[0],VIEW[1]),0,32)
 pygame.display.set_caption("anger birbs")
 clock = pygame.time.Clock()
 
+#need to redo all art at the end
 background_art = pygame.image.load("anger_art/background.png").convert_alpha()
 redwing_art = pygame.image.load("anger_art/redwing.png").convert_alpha()
 greatblue_art = pygame.image.load("anger_art/greatblue.png").convert_alpha()
@@ -43,54 +39,98 @@ blackcap_art = pygame.image.load("anger_art/blackcap.png").convert_alpha()
 ground_art = pygame.image.load("anger_art/ground.png").convert_alpha()
 log_long_art = pygame.image.load("anger_art/log_long.png").convert_alpha()
 log_short_art = pygame.image.load("anger_art/log_short.png").convert_alpha()
+slingshot_art = pygame.image.load("anger_art/slingshot2.png").convert_alpha()
 
 world = world(gravity=(0,-10), doSleep=True)
 
 things = []
+birds = []
 
-ground = Thing(world,ground_art,(5,.3),0,BOX,static=True)
+ground = Thing(world,ground_art,(5,0),0,BOX,static=True)
 things.append(ground)
-greatblue = Thing(world,greatblue_art,(8,5),-10,CIRCLE,radius=.25)
-things.append(greatblue)
+redwing = Bird(world,redwing_art,(2.2,5),-10,CIRCLE,scale=.8)
+things.append(redwing)
+birds.append(redwing)
+blackcap = Bird(world,blackcap_art,(4,5),-10,CIRCLE,scale=.8)
+things.append(blackcap)
+birds.append(blackcap)
+slingshot = Slingshot(slingshot_art,(1.5,1.1),world,scale = 1)
+things.append(slingshot)
 #a_log = Thing(world,log_long_art,(4,5),20,BOX)
 #things.append(a_log)
 
 def make_log(pos,rotation,size):
     if size == 0:
-        a_log = Thing(world,log_short_art,pos,rotation,BOX)
+        a_log = Thing(world,log_short_art,pos,rotation,BOX,scale=.8)
     elif size == 1:
-        a_log = Thing(world,log_long_art,pos,rotation,BOX)
+        a_log = Thing(world,log_long_art,pos,rotation,BOX,scale=.8)
     things.append(a_log)
 
-logs = [((1.5,1.4),90,1),((2.5,1.4),90,1),((2.0,2.4),0,1),((2.25,3.0),90,0),
-    ((1.75,3.0),90,0),((2.0,3.4),0,0)]
+logs = [((8.0,1),90,1),((9.0,1),90,1),((8.5,2),0,1),((8.2,2.5),90,0),
+    ((8.7,2.5),90,0),((8.5,3.0),0,0)]
 for log in logs:
     make_log(log[0],log[1],log[2])
 
 
-
-
+in_sling = None
+time_shot = -1000
+clicked = False
 
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if art == True:
-                    art = False
-                elif art == False:
-                    art = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            pos = pos[0]/PPM,(600-pos[1])/PPM
+            if in_sling != None:
+                if in_sling.fix.TestPoint(pos):
+                    clicked = True
+        elif event.type == pygame.MOUSEBUTTONUP and clicked:
+            clicked = False
+            in_sling.launch(screen,world,slingshot)
+            birds.remove(in_sling)
+            in_sling = None
+            time_shot = pygame.time.get_ticks()
+
+    if in_sling == None:
+        if len(birds)>0 and pygame.time.get_ticks()-time_shot >= 1000:
+            birds[0].load(world,slingshot)
+            in_sling = birds[0]
+            birds[0].body.awake = False
+    else:
+        if clicked:
+            pos = pygame.mouse.get_pos()
+            pos = pos[0]/PPM,(600-pos[1])/PPM
+            in_sling.body.transform = (pos,in_sling.body.angle)
+
+
     world.Step(TIME_STEP, 10, 10) #always do before drawing!!
     if art:
-        screen.blit(background_art,(0,0))
+        screen.fill((147,211,246))
+        screen.blit(background_art,(0,-50))
+        slingshot.draw(screen)
         for each in things:
             each.draw(screen)
     elif not art:
         screen.fill((0,0,0))
         for each in things:
             each.draw_shape(screen)
+        if in_sling != None:
+            pygame.draw.line(screen,WHITE,(slingshot.anchora.position[0]*PPM,
+                600-slingshot.anchora.position[1]*PPM),
+                (in_sling.body.position[0]*PPM,
+                600-in_sling.body.position[1]*PPM))
+            pygame.draw.line(screen,WHITE,(slingshot.anchorb.position[0]*PPM,
+                600-slingshot.anchorb.position[1]*PPM),
+                (in_sling.body.position[0]*PPM,
+                600-in_sling.body.position[1]*PPM))
+        else:
+            pygame.draw.line(screen,WHITE,(slingshot.anchora.position[0]*PPM,
+                600-slingshot.anchora.position[1]*PPM),
+                (slingshot.anchorb.position[0]*PPM,
+                600-slingshot.anchorb.position[1]*PPM))
 
     pygame.display.flip()
     clock.tick(FPS)
--- anger_sprites.py
+++ anger_sprites.py
@@ -11,8 +11,9 @@ BLACK = (0,0,0)
 PPM = 100
 
 class Thing():
-    def __init__(self,world,img,pos,rotation,shape,radius=0,static=False):
-        #img = pygame.transform.rotozoom(img,0,.75)
+    def __init__(self,world,img,pos,rotation,
+        shape,static=False,scale=1):
+        img = pygame.transform.rotozoom(img,0,scale)
         self.img = img
         angle = rotation * (pi/180)
         self.shape = shape
@@ -22,14 +23,15 @@ class Thing():
             self.body = world.CreateDynamicBody(position=(pos),
                 fixedRotation=False,angle=angle)
         if shape == CIRCLE:
+                radius = self.img.get_rect().width*.8/2/PPM
                 self.fix = self.body.CreateCircleFixture(radius=radius,
-                    density=4,friction=0.3,restitution=.3)
+                    density=5,friction=0.3,restitution=.5)
                 self.radius = radius
         elif shape == BOX:
                 dimensions = (self.img.get_rect().width/(2*PPM),
                     self.img.get_rect().height/(2*PPM))
                 self.fix = self.body.CreatePolygonFixture(box=dimensions,
-                    density=4,friction=0.3,restitution=.3)
+                    density=4,friction=0.3,restitution=.5)
     def draw(self,screen):
         angle = self.body.angle * (180/pi)
         r_img = pygame.transform.rotate(self.img,angle)
@@ -51,3 +53,38 @@ class Thing():
             y = pos[1]-radius*math.sin(self.body.angle)
             point = x,y
             pygame.draw.line(screen,(0,0,0),pos,point)
+
+class Bird(Thing):
+    def launch(self,screen,world,slingshot):
+        posa = self.body.position
+        posb = slingshot.rect.centerx,slingshot.rect.y
+        posb = posb[0]/PPM, (600-posb[1])/PPM
+        length = (((posb[0]-posa[0])**2+
+            (posb[1]-posa[1])**2)**(1/2))
+        reduct = (5/length)
+        vector = ((posb[0]-posa[0])*reduct,
+            (posb[1]-posa[1])*reduct)
+        self.body.ApplyLinearImpulse(vector,self.body.position,True)
+    def load(self,world,slingshot):
+        pos = slingshot.rect.center
+        pos = pos[0]/PPM, (600-pos[1])/PPM
+        self.body.transform = (pos,self.body.angle)
+
+class Slingshot():
+    def __init__(self,img,pos,world,scale=1):
+        img = pygame.transform.rotozoom(img,0,scale)
+        self.img = img
+        self.rect = img.get_rect(center = (pos[0]*PPM, 600-pos[1]*PPM))
+        anchora = ((self.rect.topleft[0]+10)/PPM,
+            (600-self.rect.topleft[1]+10)/PPM)
+        anchorb = ((self.rect.topright[0]-10)/PPM,
+            (600-self.rect.topright[1]+10)/PPM)
+        self.anchora = world.CreateStaticBody(position=(anchora),angle=0)
+        self.anchorb = world.CreateStaticBody(position=(anchorb),angle=0)
+    def draw(self,screen):
+        screen.blit(self.img,self.rect.topleft)
+    def draw_shape(self,screen):
+        pygame.draw.circle(screen,WHITE,(int(self.anchora.position[0]*PPM),
+            int(600-self.anchora.position[1]*PPM)),5)
+        pygame.draw.circle(screen,WHITE,(int(self.anchorb.position[0]*PPM),
+            int(600-self.anchorb.position[1]*PPM)),5)
